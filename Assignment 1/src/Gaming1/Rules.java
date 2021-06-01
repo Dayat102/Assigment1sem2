2daaa1a998357ce97e0b4bc5cbd8a57c59a490e2
ASUS <ASUS@LAPTOP-9PJNQ24C>
2021-06-02 04:12:02 +0800

    assignment

---
+++ Assignment 1/src/Gaming1/Rules.java
@@ -0,0 +1,25 @@
+package Gaming1;
+
+public class Rules {
+	
+
+	int price1;
+	String date1;
+	public void rules(int price,String date) {
+		this.price1=price;
+		this.date1=date;
+		System.out.println();
+		System.out.println("*******************************************************************");
+		System.out.println("*                    GAMING DESCRIPTION!!!!                       *");
+		System.out.println("*              -------------------------------                    *");
+		System.out.println("*                                                                 *");
+		System.out.println("*          OPEN TO ALL REGARDLESS OF GENDER AND ALSO AGE          *");
+		System.out.println("*                    FEE : RM "+this.price1 +"PER PERSON                      *");
+		System.out.println("*                      TIME:"+this.date1+"                         *");
+		System.out.println("*                                                                 *");
+		System.out.println("*                                                                 *");
+		System.out.println("*******************************************************************");
+	}
+
+
+}
